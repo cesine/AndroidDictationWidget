c42e95b07836e1cf17133272c8ec94ae52e20d59
cesine <cesine@host>
2011-04-22 12:41:09 -0400

    added database and provider for aublog histories

---
+++ src/ca/ilanguage/aublog/db/AuBlogHistoryDatabase.java
@@ -0,0 +1,143 @@
+package ca.ilanguage.aublog.db;
+
+import java.util.regex.Pattern;
+
+import android.app.SearchManager;
+import android.content.ContentProvider;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+
+public class AuBlogHistoryDatabase {
+	 /**
+     * Special value for {@link SyncColumns#UPDATED} indicating that an entry
+     * has never been updated, or doesn't exist yet.
+     */
+    public static final long UPDATED_NEVER = -2;
+
+    /**
+     * Special value for {@link SyncColumns#UPDATED} indicating that the last
+     * update time is unknown, usually when inserted from a local file source.
+     */
+    public static final long UPDATED_UNKNOWN = -1;
+
+    public interface SyncColumns {
+    	String UPDATED = "updated";
+    }
+    /**
+     * An interface which states the columns in the AuBlog History table
+     */
+    interface AuBlogHistoryColumns{
+	    String ENTRY_TITLE  ="title";
+	    String ENTRY_LABELS = "labels";
+	    String ENTRY_CONTENT = "content";
+	    String PUBLISHED_IN = "publishedin";
+	    //comma seperated list of ordered pairs, of blog entry id and audiofile corresponding directly to that entry)
+	    String AUDIO_FILES = "audiofiles";
+	    //arrange entry histories in a heirarchy based on edits
+	    String PARENT_ENTRY = "parententry";
+	    String DAUGHTER_ENTRY = "daughterentry";
+	    String PUBLISHED = "published";
+	    String TIME_CREATED = "timecreated";
+	    String TIME_EDITED = "timeedited";
+	    String LAST_MODIFIED = "lastmodified";
+    }
+    
+    public static final String AUTHORITY = "ca.ilanguage.aublog.provider.AuBlogHistory";
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+//    private static final String PATH_AUBLOG_HISTORY = "aubloghistory";
+    public static final String AUBLOG_HISTORY_TABLE_NAME = "aubloghistory";
+    
+
+    //private static final String PATH_AUBLOG_ENTRY = "aublogentrydb";
+//    private static final String PATH_AUBLOG_AUDIO = "aublogaudiodb";
+    
+    
+    private static final String PATH_EXPORT = "export";
+    private static final String PATH_SEARCH = "search";
+    private static final String PATH_SEARCH_SUGGEST = "search_suggest_query";
+
+    /**
+     * Changes in entrys are saved in the database so that the user looses no data, the entrys are linked in a
+     * heirarchy so that the previous and following versions can be found.
+     * 
+     * The history entries have the Columns defined in the interface above, as well as the BASE columns from android
+     */
+    public static class AuBlogHistory implements AuBlogHistoryColumns, BaseColumns{
+    	//leads to the database file on the data directory of the device
+    	public static final Uri CONTENT_URI =
+    		BASE_CONTENT_URI.buildUpon().appendPath(AUBLOG_HISTORY_TABLE_NAME).build();
+    	public static final Uri CONTENT_EXPORT_URI =
+    		CONTENT_URI.buildUpon().appendPath(PATH_EXPORT).build();
+    	
+    	public static final String CONTENT_TYPE = 
+        	"vnd.android.cursor.dir/vnd.ilanguage.aubloghistory";
+    	public static final String CONTENT_ITEM_TYPE =
+    		"vnd.android.cursor.item/vnd.openlanguage.ilanguage.aubloghistory";
+    	
+    	/** Default "ORDER BY" clause is by date modified */
+        public static final String DEFAULT_SORT_ORDER = AuBlogHistoryColumns.LAST_MODIFIED+ " DESC";
+        //for alphabetical by title
+        //public static final String DEFAULT_SORT = VendorsColumns.NAME + " COLLATE NOCASE ASC";
+        
+
+        /*
+    	 * Needed for searchable Modules
+    	 */
+    	public static Uri buildSearchUri(String query){
+    		return CONTENT_URI.buildUpon().appendPath(PATH_SEARCH).appendPath(query).build();
+    	}
+    	public static boolean isSearchUri(Uri uri){
+    		//1 refers to the "search" position added in the buildSearchUri funciton above
+    		return PATH_SEARCH.equals(uri.getPathSegments().get(1));
+    	}
+    	public static String getSearchQuery(Uri uri){
+    		//2 refers to the "query" position added in the buildSearchUri function above
+    		return uri.getPathSegments().get(2);
+    	}
+    	public static final String SEARCH_SNIPPET = "search_snippet";
+
+        
+    }
+    /*
+     * General classes and useful functions to be used with the Domain classes
+     */
+    public static class SearchSuggest{
+    	public static final Uri CONTENT_URI = 
+    		BASE_CONTENT_URI.buildUpon().appendPath(PATH_SEARCH_SUGGEST).build();
+    	public static final String DEFAULT_SORT = SearchManager.SUGGEST_COLUMN_TEXT_1
+    		+ " COLLATE NOCASE ASC";
+    }
+    /**
+     * * Sanitize the given string to be {@link Uri} safe for building
+     * {@link ContentProvider} paths. For more info see best practices in ioschedule utils package.
+     * @param input to become a uri
+     * @return output safe to become a uri (numbers letters underscores and hyphens)
+     */
+    public static String santizeId(String input){
+    	return sanitizeId(input, false);
+    }
+    /**
+     * * Sanitize the given string to be {@link Uri} safe for building. Allows client to specify if 
+     * (parenthetical expressions) should be removed from the string.
+     * 
+     * {@link ContentProvider} paths.
+     * @param input to become a uri
+     * @return output safe to become a uri (numbers letters underscores and hyphens)
+     */
+    public static String sanitizeId(String input, boolean stripParentheses){
+    	/** Used to sanitize a string to be {@link Uri} safe. */
+        Pattern sSanitizePattern = Pattern.compile("[^a-z0-9-_]");
+        Pattern sParenthesesPattern = Pattern.compile("\\(.*?\\)");
+
+    	if (input == null) return null;
+    	if (stripParentheses) {
+    		//Strip out all parenthetical statements when requested
+    		input = sParenthesesPattern.matcher(input).replaceAll("");
+    	}
+    	return sSanitizePattern.matcher(input.toLowerCase()).replaceAll("");
+    }
+    //null constructor
+    private AuBlogHistoryDatabase(){}
+    
+}
---
+++ src/ca/ilanguage/aublog/db/AuBlogHistoryProvider.java
@@ -0,0 +1,297 @@
+package ca.ilanguage.aublog.db;
+
+import java.util.HashMap;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.provider.LiveFolders;
+import android.text.TextUtils;
+import android.util.Log;
+import ca.ilanguage.aublog.db.AuBlogHistoryDatabase;
+import ca.ilanguage.aublog.db.AuBlogHistoryDatabase.AuBlogHistory;
+
+
+public class AuBlogHistoryProvider extends ContentProvider {
+
+    private static final String TAG = "AuBlogHistoryProvider";
+
+    private static final String DATABASE_NAME = "aubloghistory.db";
+    private static final int DATABASE_VERSION = 1;
+    //private static final String  AUBLOG_HISTORY_TABLE_NAME= AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME;
+
+    private static HashMap<String, String> sAuBlogHistoryProjectionMap;
+    private static HashMap<String, String> sLiveFolderProjectionMap;
+
+    //retrieve options?
+    private static final int AUBLOGHISTORIES = 1;
+    private static final int AUBLOGHISTORY_ID = 2;
+    private static final int LIVE_FOLDER_AUBLOGHISTORYS = 3;
+
+    private static final UriMatcher sUriMatcher;
+    
+    /**
+     * This class helps open, create, and upgrade the database file.
+     */
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE " + AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME + " ("
+            		+ AuBlogHistory._ID+ " INTEGER PRIMARY KEY,"
+            		+ AuBlogHistory.PUBLISHED + " INTEGER,"
+            		+ AuBlogHistory.ENTRY_CONTENT + " TEXT,"
+            		+ AuBlogHistory.ENTRY_LABELS + " TEXT,"
+            		+ AuBlogHistory.ENTRY_TITLE + " TEXT,"
+            		+ AuBlogHistory.PARENT_ENTRY + " INTEGER,"
+            		+ AuBlogHistory.DAUGHTER_ENTRY   + " INTEGER,"
+            		+ AuBlogHistory.PUBLISHED_IN + " TEXT,"
+            		+ AuBlogHistory.AUDIO_FILES + " TEXT,"
+            		+ AuBlogHistory.LAST_MODIFIED + " INTEGER,"
+            		+ AuBlogHistory.TIME_CREATED + " INTEGER,"
+            		+ AuBlogHistory.TIME_EDITED + " INTEGER"
+            		+ ");");
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        	Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+        			+ newVersion + ", which will destroy all old data");
+        	db.execSQL("DROP TABLE IF EXISTS "+ AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME);
+        	onCreate(db);
+        }
+    }//end databasehelper
+    private DatabaseHelper mOpenHelper;
+
+	@Override
+	public int delete(Uri uri, String where, String[] whereArgs) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int count;
+        switch (sUriMatcher.match(uri)) {
+        case AUBLOGHISTORIES:
+            count = db.delete(AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME, where, whereArgs);
+            break;
+
+        case AUBLOGHISTORY_ID:
+            String itemId = uri.getPathSegments().get(1);
+            count = db.delete(AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME, AuBlogHistory._ID + "=" + itemId
+                    + (!TextUtils.isEmpty(where) ? " AND (" + where + ')' : ""), whereArgs);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+	}
+
+	@Override
+	public String getType(Uri uri) {
+		switch (sUriMatcher.match(uri)) {
+        case AUBLOGHISTORIES:
+        case LIVE_FOLDER_AUBLOGHISTORYS:
+            return AuBlogHistory.CONTENT_TYPE;
+
+        case AUBLOGHISTORY_ID:
+            return AuBlogHistory.CONTENT_ITEM_TYPE;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues initialValues) {
+//initial values are null
+    	
+    	
+        // Validate the requested uri
+        if (sUriMatcher.match(uri) != AUBLOGHISTORIES) {
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        ContentValues values;
+        if (initialValues != null) {
+            values = new ContentValues(initialValues);
+        } else {
+            values = new ContentValues();
+        }
+
+        Long now = Long.valueOf(System.currentTimeMillis());
+
+        // Make sure that the fields are all set
+        if (values.containsKey(AuBlogHistory.TIME_CREATED) == false) {
+            values.put(AuBlogHistory.TIME_CREATED, now);
+        }
+        if (values.containsKey(AuBlogHistory.LAST_MODIFIED) == false) {
+            values.put(AuBlogHistory.LAST_MODIFIED, now);
+        }
+        if (values.containsKey(AuBlogHistory.TIME_EDITED) == false) {
+            values.put(AuBlogHistory.TIME_EDITED, "0");
+        }
+        if (values.containsKey(AuBlogHistory.PUBLISHED) == false) {
+            values.put(AuBlogHistory.PUBLISHED, "0");
+        }       
+ 
+
+      // initialize nullable fields here
+        if (values.containsKey(AuBlogHistory.ENTRY_TITLE) == false) {
+//            Resources r = Resources.getSystem();
+            values.put(AuBlogHistory.ENTRY_TITLE, "");// r.getString(android.R.string.untitled_blog_title));
+        }
+        if (values.containsKey(AuBlogHistory.ENTRY_CONTENT) == false) {
+            values.put(AuBlogHistory.ENTRY_CONTENT, "");
+        }       
+        if (values.containsKey(AuBlogHistory.ENTRY_LABELS) == false) {
+            values.put(AuBlogHistory.ENTRY_LABELS, "");
+        }       
+        if (values.containsKey(AuBlogHistory.PARENT_ENTRY) == false) {
+            values.put(AuBlogHistory.PARENT_ENTRY, "");
+        }  
+        if (values.containsKey(AuBlogHistory.DAUGHTER_ENTRY) == false) {
+            values.put(AuBlogHistory.DAUGHTER_ENTRY, "");
+        }       
+     
+        if (values.containsKey(AuBlogHistory.AUDIO_FILES) == false) {
+            values.put(AuBlogHistory.AUDIO_FILES, "");
+        }
+        if (values.containsKey(AuBlogHistory.PUBLISHED_IN) == false) {
+            values.put(AuBlogHistory.PUBLISHED_IN, "");
+        }       
+       
+        
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        // it seems suspicious to only be the content of PARENT_ENTRY, ah its the nullcolumnhack
+        
+        long rowId = db.insert(AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME, AuBlogHistory.PARENT_ENTRY, values);
+        if (rowId > 0) {
+            Uri audiobookUri = ContentUris.withAppendedId(AuBlogHistory.CONTENT_URI, rowId);
+            getContext().getContentResolver().notifyChange(audiobookUri, null);
+            return audiobookUri;
+        }
+
+        throw new SQLException("Failed to insert row into " + uri);
+	}
+
+	@Override
+	public boolean onCreate() {
+		mOpenHelper = new DatabaseHelper(getContext());
+        return true;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME);
+
+        switch (sUriMatcher.match(uri)) {
+        case AUBLOGHISTORIES:
+        	//gets a cursor of all rows, with all columns (all should be entered into the projectionmap)
+            qb.setProjectionMap(sAuBlogHistoryProjectionMap);
+            break;
+
+        case AUBLOGHISTORY_ID:
+            qb.setProjectionMap(sAuBlogHistoryProjectionMap);
+            //get the row (of selected columns in projetion, it should be all of them) for that ID
+            //gets a cursor of the row which matches the id from the uri
+            qb.appendWhere(AuBlogHistory._ID + "=" + uri.getPathSegments().get(1));
+            break;
+
+        case LIVE_FOLDER_AUBLOGHISTORYS:
+        	//gets a cursor of all rows, but with just a few columns
+            qb.setProjectionMap(sLiveFolderProjectionMap);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        // If no sort order is specified use the default
+        String orderBy;
+        if (TextUtils.isEmpty(sortOrder)) {
+            orderBy = AuBlogHistory.DEFAULT_SORT_ORDER;
+        } else {
+            orderBy = sortOrder;
+        }
+
+        // Get the database and run the query
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, orderBy);
+
+        // Tell the cursor what uri to watch, so it knows when its source data changes
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int count;
+        switch (sUriMatcher.match(uri)) {
+        case AUBLOGHISTORIES:
+            count = db.update(AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME, values, selection, selectionArgs);
+            break;
+
+        case AUBLOGHISTORY_ID:
+            String audiobookId = uri.getPathSegments().get(1);
+            //update teh row using the values provided
+            //this takes updates from the title editor
+            //this takes updates from the audiobooks editor
+            count = db.update(AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME, values, AuBlogHistory._ID + "=" + audiobookId
+                    + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+            break;
+
+        default:
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+	}
+	static {
+	        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+	        sUriMatcher.addURI(AuBlogHistoryDatabase.AUTHORITY, AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME, AUBLOGHISTORIES);
+	        sUriMatcher.addURI(AuBlogHistoryDatabase.AUTHORITY, AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME+"/#", AUBLOGHISTORY_ID);
+	        sUriMatcher.addURI(AuBlogHistoryDatabase.AUTHORITY, "live_folders/"+AuBlogHistoryDatabase.AUBLOG_HISTORY_TABLE_NAME, LIVE_FOLDER_AUBLOGHISTORYS);
+
+	        sAuBlogHistoryProjectionMap = new HashMap<String, String>();
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory._ID, AuBlogHistory._ID);
+	        
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.ENTRY_TITLE, AuBlogHistory.ENTRY_TITLE);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.ENTRY_CONTENT, AuBlogHistory.ENTRY_CONTENT);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.ENTRY_LABELS, AuBlogHistory.ENTRY_LABELS);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.PUBLISHED, AuBlogHistory.PUBLISHED);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.PUBLISHED_IN, AuBlogHistory.PUBLISHED_IN);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.PARENT_ENTRY, AuBlogHistory.PARENT_ENTRY);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.DAUGHTER_ENTRY, AuBlogHistory.DAUGHTER_ENTRY);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.LAST_MODIFIED, AuBlogHistory.LAST_MODIFIED);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.TIME_CREATED, AuBlogHistory.TIME_CREATED);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.TIME_EDITED, AuBlogHistory.TIME_EDITED);
+	        sAuBlogHistoryProjectionMap.put(AuBlogHistory.AUDIO_FILES, AuBlogHistory.AUDIO_FILES);
+
+	        // Support for Live Folders.
+	        sLiveFolderProjectionMap = new HashMap<String, String>();
+	        sLiveFolderProjectionMap.put(LiveFolders._ID, AuBlogHistory._ID + " AS " +
+	                LiveFolders._ID);
+	        sLiveFolderProjectionMap.put(LiveFolders.NAME, AuBlogHistory.ENTRY_TITLE + " AS " +
+	                LiveFolders.NAME);
+	        sLiveFolderProjectionMap.put(LiveFolders.DESCRIPTION, AuBlogHistory.ENTRY_CONTENT + " AS " +
+	                LiveFolders.DESCRIPTION);
+	        
+	        // Add more columns here for more robust Live Folders.
+	    }
+}
