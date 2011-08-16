e8134f0112542831f3be056d30cd5b15994d5abc
cesine <cesine@host>
2011-08-16 15:38:25 -0400

    get information from intent working

--- src/ca/ilanguage/aublog/service/NotifyingTranscriptionService.java
+++ src/ca/ilanguage/aublog/service/NotifyingTranscriptionService.java
@@ -17,17 +17,23 @@
 package ca.ilanguage.aublog.service;
 
 
+import java.util.ArrayList;
+
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.location.GpsStatus.NmeaListener;
+import android.media.AudioTrack;
 import android.os.Binder;
 import android.os.ConditionVariable;
 import android.os.IBinder;
 import android.os.Parcel;
 import android.os.RemoteException;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import ca.ilanguage.aublog.R;
 
@@ -37,17 +43,86 @@ import ca.ilanguage.aublog.R;
  * 
  */
 public class NotifyingTranscriptionService extends Service {
-    
+
+    private NotificationManager mNM;
+   
+    private AudioTrack mAudioTrack;
+	private String mAudioFilePath;
+	private int mSplitType;
+	private ArrayList<String> mTimeCodes;
+	
+	public static final String EXTRA_AUDIOFILE_FULL_PATH = "audioFilePath";
+	public static final String EXTRA_RESULTS = "splitUpResults";
+	public static final String EXTRA_SPLIT_TYPE = "splitOn";
+	
+	/**
+	 * Splitting on Silence is relatively quick it only requires mathematic calculation on the audio sample, 
+	 * this is used by default by all other split types.
+	 */
+	public static final int SPLIT_ON_SILENCE = 1;
+	/**
+	 * Subtitles should not exceed a certain length to fit on the screen, use this if you actually have the goal of generating subtitles
+	 */
+	public static final int SPLIT_ON_TOO_MANY_CHARS = 9;
+	/**
+	 * Warning: using prosodic cues requires more audio analysis (fourier transforms FFT) and so they will slow the service down substantially
+	 */
+	public static final int SPLIT_ON_ANY_PROSODIC_CUE = 2;
+
+	/*
+	 * List of Optional Prosodic cues
+	 * 
+	 * For more info: 
+	 * 		http://en.wikipedia.org/wiki/Prosody_(linguistics)
+	 * For more bleeding-edge info: 
+	 * 		Experimental and Theoretical Advances in Prosody Conference http://prosodylab.org/etap/
+	 */
+	public static final int SPLIT_ON_LENGTHENED_VOWEL = 3;
+	public static final int SPLIT_ON_LENGHTENED_CONSONANT = 4;
+	public static final int SPLIT_ON_LENGTHENED_ANYTHING = 5;
+	public static final int SPLIT_ON_GLOTTALIZATION = 6;
+	public static final int SPLIT_ON_BREATH = 7;
+	/**
+	 * For those speakers of "valley-girl" English prosodic phrases can be 
+	 * discovered using upspeak also known as: {high rising terminal (HRT), uptalk, upspeak, rising inflection or high rising intonation }
+	 * 
+	 * For more info: http://en.wikipedia.org/wiki/High_rising_terminal
+	 */
+	public static final int SPLIT_ON_UPSPEAK = 8;
+	
+
     // Use a layout id for a unique identifier
     private static int MOOD_NOTIFICATIONS = R.layout.status_bar_notifications;
+    private String mNotificationMessage;
 
     // variable which controls the notification thread 
     private ConditionVariable mCondition;
  
     @Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+    	/*
+         * TODO get data from bundle, store it in the member variables
+         */
+        try {
+            mAudioFilePath = intent.getExtras().getString(EXTRA_AUDIOFILE_FULL_PATH);
+           
+        } catch (Exception e) {
+        	//Toast.makeText(SRTGeneratorActivity.this, "Error "+e,Toast.LENGTH_LONG).show();
+        }
+        if (mAudioFilePath.length() > 0) {
+            mNotificationMessage= mAudioFilePath;
+        }else{
+        	mNotificationMessage ="No file";
+        }
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
     public void onCreate() {
+    	
         mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
+        
         // Start up the thread running the service.  Note that we create a
         // separate thread because the service normally runs in the process's
         // main thread, which we don't want to block.
@@ -67,31 +142,69 @@ public class NotifyingTranscriptionService extends Service {
     private Runnable mTask = new Runnable() {
         public void run() {
         	
+        	/*
+        	 * Send audio file for transcription to server. 
+        	 * 
+        	 */
         	
             for (int i = 0; i < 4; ++i) {
-                showNotification(R.drawable.stat_happy,  R.string.status_bar_notifications_sending_message);
+                showNotification(R.drawable.stat_happy,  mNotificationMessage);
                 if (mCondition.block(5 * 1000)) 
                     break;
-                showNotification(R.drawable.stat_happy,  R.string.status_bar_notifications_sending_message);
+                showNotification(R.drawable.stat_happy,  mNotificationMessage);
                 if (mCondition.block(5 * 1000)) 
                     break;
-                showNotification(R.drawable.stat_happy,  R.string.status_bar_notifications_sending_message);
+                showNotification(R.drawable.stat_happy,  mAudioFilePath);
                 if (mCondition.block(5 * 1000)) 
                     break;
             }
+            
+            /*
+             * Generate the SRT
+             */
+            String message = generateSRT();
+            
+            /*
+             * Return results
+             * 
+             * As an intent:
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra(EXTRA_RESULTS, mTimeCodes);
+            setResult(Activity.RESULT_OK, resultIntent);
+            finish();
+            * As a .srt file in the audio directory
+             */
+            
             // Done with our work...  stop the service!
             NotifyingTranscriptionService.this.stopSelf();
+            
+            
         }
     };
 
     @Override
     public IBinder onBind(Intent intent) {
+    	/*
+    	 * TODO unknown if it is necessary to get data from the intent here in addition to onstartcommand
+         * TODO get data from bundle, store it in the member variables
+         */
+        try {
+            mAudioFilePath = intent.getExtras().getString(EXTRA_AUDIOFILE_FULL_PATH);
+           
+        } catch (Exception e) {
+        	//Toast.makeText(SRTGeneratorActivity.this, "Error "+e,Toast.LENGTH_LONG).show();
+        }
+        if (mAudioFilePath.length() > 0) {
+            mNotificationMessage= mAudioFilePath;
+        }else{
+        	mNotificationMessage ="No file";
+        }
         return mBinder;
     }
     
-    private void showNotification(int moodId, int textId) {
+    private void showNotification(int moodId, String message) {
         // In this sample, we'll use the same text for the ticker and the expanded notification
-        CharSequence text = getText(textId);
+        CharSequence text = message;
 
         // Set the icon, scrolling text and timestamp.
         // Note that in this example, we pass null for tickerText.  We update the icon enough that
@@ -105,14 +218,52 @@ public class NotifyingTranscriptionService extends Service {
                 new Intent(this, NotifyingController.class), 0);
 
         // Set the info for the views that show in the notification panel.
-        notification.setLatestEventInfo(this, "notifications_mood_title",
+        notification.setLatestEventInfo(this, "Transcription in background",
                        text, contentIntent);
 
         // Send the notification.
         // We use a layout id because it is a unique number.  We use it later to cancel.
         mNM.notify(MOOD_NOTIFICATIONS, notification);
     }
+    public String generateSRT(){
+    	String messageToReturn = "";
+    	/*
+    	 * TODO 
+    	 * make sure the audioTrack is open
+    	 */
 
+    	/* 
+    	 * TODO 
+    	 * implement switch on mSplitType
+    	 */
+    	switch (mSplitType){
+    		default:
+    			messageToReturn = splitOnSilence();
+    			break;
+    	}
+    	
+    	
+    	return messageToReturn;
+    }
+    public String splitOnSilence(){
+    	mTimeCodes = new ArrayList<String>();
+    	mTimeCodes.add("0:00:02.350,0:00:06.690");
+    	mTimeCodes.add("0:00:07.980,0:00:12.780");
+    	mTimeCodes.add("0:00:14.529,0:00:17.970");
+    	mTimeCodes.add("0:00:17.970,0:00:20.599");
+    	return "right now, these are fake timecodes";
+    }
+    public String testSplitOnSilence(){
+    	/*
+    	 * TODO
+    	 * open raw/res/...wav sample
+    	 * set the audioTrack to that file
+    	 * run splitOnSilence on it
+    	 * compare the output against the raw/res...corrected.sbv time stamps
+    	 * return test failed/passed
+    	 */
+    	return "fail";
+    }
     // This is the object that receives interactions from clients.  See
     // RemoteService for a more complete example.
     private final IBinder mBinder = new Binder() {
@@ -122,6 +273,4 @@ public class NotifyingTranscriptionService extends Service {
             return super.onTransact(code, data, reply, flags);
         }
     };
-
-    private NotificationManager mNM;
 }
