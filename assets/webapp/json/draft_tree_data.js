e9ea7df3c5cd0323bd3c437a47470c7fad83160a
cesine <cesine@host>
2012-02-10 23:14:57 -0500

    restructuring webapp for issue #14

---
+++ assets/webapp/json/draft_tree_data.js
@@ -0,0 +1,36 @@
+var draftTreeData={id: "1",
+name: "Root",
+hidden: "0",
+data: { content:"empty"},
+children: [{
+id: "2",
+name: "About me",
+hidden: "0",
+data: { content:"Your blog&apos;s About Me page should not be overlooked. It&apos;s an essential tool to establish who you are as a blogger and help readers understand what your blog is about. Simply listing your name and contact information is not enough. Sell yourself and your blog on your About Me page, and make readers believe you&apos;re not only an expert in your blog&apos;s topic but your blog is also the place for people to find information about your topic on the web. From &lt;a href=&apos;http://weblogs.about.com/od/partsofablog/qt/AboutPage.htm&apos;&gt;About.com&lt;/a&gt;"},
+children: [{
+id: "3",
+name: "About my blog",
+hidden: "0",
+data: { content:"&lt;p&gt;Your blog&apos;s About Me page should not be overlooked. It&apos;s an essential tool to establish who you are as a blogger and help readers understand what your blog is about. Simply listing your name and contact information is not enough. Sell yourself and your blog on your About Me page, and make readers believe you&apos;re not only an expert in your blog&apos;s topic but your blog is also the place for people to find information about your topic on the web. From &lt;a href=&apos;http://weblogs.about.com/od/partsofablog/qt/AboutPage.htm&apos;&gt;About.com&lt;/a&gt;&lt;/p&gt;Following are the three most important elements to include on your About Me page:Your experience and why you&apos;re the best person to blog about your subject matterLinks to your other websites or blogs self promotion is critical to your success as a bloggerYour contact information so interested readers can ask questions or reach out to you for other business opportunities (which happens often in the blogosphere)"},
+children: []
+} ]
+} ,{
+id: "4",
+name: "How to use the drafts tree",
+hidden: "0",
+data: { content:"The drafts tree lets you visualize/listen to blog entries, and re-draft or re-wind until you are ready to publish. Each time you hit the save button, a new node is created. "},
+children: [{
+id: "5",
+name: "Use via recording...",
+hidden: "0",
+data: { content:"You can brainstorm and build your blog entry by recording your voice, and listening to it. Once you have a blog entry that you are satisfied with you can send it for transcription. Then you can edit it in text form and publish it. Alternatively, you can use AuBlog as a traditional blog client by just typing in the Edit Entry page."},
+children: []
+} ,{
+id: "6",
+name: "Use via typing...",
+hidden: "0",
+data: { content:"You can also simply type your blog entry by clicking on the Edit button."},
+children: []
+} ]
+} ]
+};
