5874941da739cbb01b19e022e463f02525fe8d43
cesine <cesine@yahoo.com>
2012-02-22 22:32:25 -0500

    added record button

--- src/ca/ilanguage/dictation/widget/activity/SampleHost.java
+++ src/ca/ilanguage/dictation/widget/activity/SampleHost.java
@@ -1,7 +1,20 @@
 package ca.ilanguage.dictation.widget.activity;
 
+import ca.ilanguage.dictation.widget.R;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SampleHost extends Activity {
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dictation_widget);
+        
 
+	}
+	public void onRecordClick(View v) {
+		Toast.makeText(this, "Record.", Toast.LENGTH_LONG).show();
+	}
 }
